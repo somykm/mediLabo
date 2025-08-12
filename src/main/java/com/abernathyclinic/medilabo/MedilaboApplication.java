737aaa5e8dd1a9861d5953588c79584828074930
https://github.com/somykm <sk43@hood.edu>
2025-08-11 22:45:50 -0400

    basic dependencies has been added

---
+++ src/main/java/com/abernathyclinic/medilabo/MedilaboApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abernathyclinic/medilabo/MedilaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
